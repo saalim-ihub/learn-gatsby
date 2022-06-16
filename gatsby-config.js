ab73455af773572f633f3af56b6457032eea9c4d
saalim-ihub <saalim.raza@ihub-data.iiit.ac.in>
2022-06-16 07:10:19 +0530

    added protein structures

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: `LearnGatsby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Homepage = () => {
   return (
-    <Layout pageTitle="Home" pageHeading="Welcome to homepage">
-      <p>Welcome again!</p>
+    <Layout pageTitle="Home" pageHeading="Welcome to my homepage">
+      <p>This is an image of protein!</p>
+      <StaticImage src="../images/fancyProtein.png" alt="A fancy Protein" />
+      <StaticImage src="../images/Myoglobin.png" alt="Structure of Myoglobin" />
     </Layout>
   );
 };
